0b03c259b22fef476a562283a60a92fa35f2a3a9
Neox06_Dev <nicolas.gonzalez01@davinci.edu.ar>
2026-06-26 16:35:49 -0300

    feat: integracion de barra de busqueda, cards y actualizacion de la home

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <RouterLink :to="{ name: 'detalle-pelicula', params: { id: pelicula.id } }" class="tarjeta">
+    <img
+      v-if="pelicula.poster_path"
+      :src="`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`"
+      :alt="pelicula.title"
+      class="tarjeta__poster"
+    />
+    <div class="tarjeta__info">
+      <h3 class="tarjeta__titulo">{{ pelicula.title }}</h3>
+      <span class="tarjeta__rating"> {{ pelicula.vote_average?.toFixed(1) }}</span>
+      <span class="tarjeta__anio">{{ pelicula.release_date?.slice(0, 4) }}</span>
+    </div>
+  </RouterLink>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+
+defineProps({
+  pelicula: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="buscador">
+    <input
+      v-model="consulta"
+      type="text"
+      placeholder="Buscar película..."
+      class="buscador__input"
+      @input="alEscribir"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const emit = defineEmits(['buscar'])
+
+const consulta = ref('')
+
+const alEscribir = () => {
+  emit('buscar', consulta.value)
+}
+</script>
