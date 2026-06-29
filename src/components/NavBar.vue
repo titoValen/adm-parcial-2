e705433a913c763ed23fe890d707ed6a2b16cfe2
titoValen <valentin.tito@davinci.edu.ar>
2026-06-29 16:28:18 -0300

    feat: agregar estilos dinámicos a las etiquetas de la barra de navegación

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,15 +2,15 @@
   <nav class="navbar">
     <RouterLink :to="{ name: 'inicio' }" class="navbar__item">
       <IconHome width="24" height="24" :color="rutaActual === 'inicio' ? '#6e2fe7' : '#7F7F7F'" />
-      <span class="navbar__label">Inicio</span>
+      <span class="navbar__label" :style="{ color: rutaActual === 'inicio' ? '#6e2fe7' : '#7F7F7F' }">Inicio</span>
     </RouterLink>
     <RouterLink :to="{ name: 'buscar' }" class="navbar__item">
       <IconSearch width="24" height="24" :color="rutaActual === 'buscar' ? '#6e2fe7' : '#7F7F7F'" />
-      <span class="navbar__label">Buscar</span>
+      <span class="navbar__label" :style="{ color: rutaActual === 'buscar' ? '#6e2fe7' : '#7F7F7F' }">Buscar</span>
     </RouterLink>
     <RouterLink :to="{ name: 'favoritos' }" class="navbar__item">
       <IconFavorite width="24" height="24" :color="rutaActual === 'favoritos' ? '#6e2fe7' : '#7F7F7F'" />
-      <span class="navbar__label">Favoritos</span>
+      <span class="navbar__label" :style="{ color: rutaActual === 'favoritos' ? '#6e2fe7' : '#7F7F7F' }">Favoritos</span>
     </RouterLink>
   </nav>
 </template>
