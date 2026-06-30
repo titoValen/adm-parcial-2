a8e3e707a6fcb4d303c910d8e45da07f29a1e751
titoValen <valentin.tito@davinci.edu.ar>
2026-06-30 15:51:10 -0300

    feat: agregar componente Header.vue con lógica de navegación y visualización de rutas

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <template v-if="rutaActual === 'buscar'">
+    <header class="header">
+      <h1 class="header__title">Buscar</h1>
+      <p class="header__subtitle">Encontra tu próxima película favorita</p>
+    </header>
+  </template>
+  <template v-else-if="rutaActual === 'favoritos'">
+    <header class="header">
+      <h1 class="header__title">Favoritos</h1>
+      <p class="header__subtitle">Tus películas favoritas en un solo lugar</p>
+    </header>
+  </template>
+  <template v-else-if="rutaActual === 'inicio'">
+    <header class="header">
+      <h1 class="header__title">
+        <figure><img src="@/assets/icon/logo_text.svg" alt="Icono de Flicka con su icono y texto"></figure>
+      </h1>
+    </header>
+  </template>
+</template>
+
+<script setup>
+defineOptions({ name: 'AppHeader' })
+
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const ruta = useRoute()
+const rutaActual = computed(() => ruta.name)
+</script>
+
+<style scoped></style>
