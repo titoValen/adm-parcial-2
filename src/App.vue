02cf8f6259eb814fd4b35218459a54184bb140e8
titoValen <valentin.tito@davinci.edu.ar>
2026-06-30 15:51:21 -0300

    feat: agregar Header.vue al componente App.vue para mejorar la estructura de la interfaz

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import NavBar from '@/components/NavBar.vue'
+import Header from './components/Header.vue';
 </script>
 
 <template>
+  <Header />
   <NavBar />
   <RouterView />
 </template>
