021eb1f6379334fe1abd9916bab9f9dbf2d1ea1f
titoValen <valentin.tito@davinci.edu.ar>
2026-06-30 15:54:53 -0300

    feat: eliminar título de la vista de favoritos para simplificar la interfaz

--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -1,7 +1,5 @@
 <template>
     <main class="favoritos">
-        <h1 class="favoritos__titulo">Favoritos</h1>
-
         <div v-if="peliculas.length === 0" class="favoritos__vacio">
             <IconNoFavorite width="149" height="110" />
             <p class="favoritos__vacio-titulo">No tenés ninguna película en favoritos</p>
