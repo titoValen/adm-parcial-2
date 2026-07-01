8188a1e06b053a7794c325debd4a2f06dd042275
Neox06-Dev <nicolas.gonzalez01@davinci.edu.ar>
2026-07-01 13:40:11 -0300

    aca pones los cambios que se hicieron

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -24,5 +24,18 @@ defineProps({
   max-width: 500px;
   cursor: pointer;
   border-radius: 1rem;
+  box-shadow: 0 0 25px var(--color-acento);
+  background: var(--color-acento);
+  box-shadow: var(--color-acento) 0px 0px 25px;
+  transition: 0.5s;
+  transition-property: box-shadow;
+  font-family: var(--font-bold);
+}
+  
+.button:hover {
+  box-shadow: 0 0 5px var(--color-acento),
+              0 0 25px var(--color-acento),
+              0 0 50px var(--color-acento),
+              0 0 100px var(--color-acento);
 }
 </style>
--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -1,30 +1,34 @@
 <template>
-    <main class="favoritos">
-        <div v-if="peliculas.length === 0" class="favoritos__vacio">
-            <IconNoFavorite width="149" height="110" />
-            <p class="favoritos__vacio-titulo">No tenés ninguna película en favoritos</p>
-            <p class="favoritos__vacio-subtitulo">Las películas que agregues a favoritos aparecerán acá</p>
-            <RouterLink :to="{ name: 'buscar' }" class="favoritos__boton">
-                Explorar películas
-            </RouterLink>
-        </div>
+  <main class="favoritos">
+    
 
-        <div v-else class="favoritos__grilla">
-            <MovieCard v-for="pelicula in peliculas" :key="pelicula.id" :pelicula="pelicula" />
-        </div>
-    </main>
+    <div v-if="peliculas.length === 0" class="favoritos__vacio">
+      <IconNoFavorite width="149" height="110" />
+      <p class="favoritos__vacio-titulo">No tenés ninguna película en favoritos</p>
+      <p class="favoritos__vacio-subtitulo">Las películas que agregues a favoritos aparecerán acá</p>
+      <AppButton @click="irABuscar">Explorar películas</AppButton>
+    </div>
+
+    <div v-else class="favoritos__grilla">
+      <MovieCard v-for="pelicula in peliculas" :key="pelicula.id" :pelicula="pelicula" />
+    </div>
+  </main>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import { RouterLink } from 'vue-router'
+import { useRouter } from 'vue-router'
 import { obtenerFavoritos } from '@/stores/favoritos.js'
 import MovieCard from '@/components/MovieCard.vue'
+import AppButton from '@/components/Button.vue'
 import IconNoFavorite from '@/components/icons/IconNoFavorite.vue'
 
+const enrutador = useRouter()
 const peliculas = ref([])
 
+const irABuscar = () => enrutador.push({ name: 'buscar' })
+
 onMounted(() => {
-    peliculas.value = obtenerFavoritos()
+  peliculas.value = obtenerFavoritos()
 })
 </script>
