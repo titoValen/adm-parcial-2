d2fd136347f2e6e6a8bf249f17260196b850f650
Neox06-Dev <nicolas.gonzalez01@davinci.edu.ar>
2026-07-01 15:57:47 -0300

    cambios menores en el boton de agregar favoritos

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -27,5 +27,28 @@ defineProps({
   max-width: 500px;
   cursor: pointer;
   border-radius: 1rem;
+  box-shadow: 0 0 25px var(--color-acento);
+  background: var(--color-acento);
+  box-shadow: var(--color-acento) 0px 0px 25px;
+  transition: 0.5s;
+  transition-property: box-shadow;
+  font-family: var(--font-bold);  
+}
+
+.button__contenido {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  font-family: var(--font-bold);
+  font-size: 1rem
+
+}
+
+.button:hover {
+  box-shadow: 0 0 5px var(--color-acento),
+              0 0 25px var(--color-acento),
+              0 0 50px var(--color-acento),
+              0 0 100px var(--color-acento);
 }
 </style>
