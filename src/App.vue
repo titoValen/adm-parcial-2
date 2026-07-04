a37187472b0eed948d31211e8671ff133217049d
titoValen <valentin.tito@davinci.edu.ar>
2026-07-04 13:14:12 -0300

    feat: agregar transición de opacidad al componente RouterView

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import NavBar from '@/components/NavBar.vue'
-import Header from './components/Header.vue';
+import Header from './components/Header.vue'
 </script>
 
 <template>
   <Header />
   <NavBar />
-  <RouterView />
+  <Transition>
+    <RouterView />
+  </Transition>
 </template>
+
+<style scope>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
